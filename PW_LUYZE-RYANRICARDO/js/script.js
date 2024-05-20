c309e418ae3b8717d974e8f981829cf1cbb7d28c
luyze <168435824+luyzecaetano@users.noreply.github.com>
2024-05-20 13:46:05 +0000

    campus html

---
+++ PW_LUYZE-RYANRICARDO/js/script.js
@@ -0,0 +1,16 @@
+var banners = [
+  "img/campus_banner1.jpg",
+  "img/campus_banner2.jpg",
+  "img/campus_banner3.jpg",
+  "img/campus_banner4.jpg",
+  "img/campus_banner5.jpg",
+  "img/campus_banner6.jpeg",
+];
+var currentBanner = 0;
+
+function switchBanner() {
+  currentBanner = (currentBanner + 1) % 6;
+  document.querySelector(".campus img").src = banners[currentBanner];
+}
+
+var timer = setInterval(switchBanner, 1000);
