cc0cf7be7ad827339c58b347e9e65c47f4df7afe
luyzecaetano <luyzecaetano@gmail.com>
2024-05-23 17:50:36 -0300

    validaçao v3
     quase la

--- PW_LUYZE-RYANRICARDO/js/script.js
+++ PW_LUYZE-RYANRICARDO/js/script.js
@@ -9,33 +9,51 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // verifica se o nome está vazio
-  if (nameInput.value === "") {
-    alert("Por favor, preencha o seu nome.");
+  if (nameInput.value === "" || nameInput.value === " ") {
+    nameInput.placeholder = "Campo Obrigatório";
+    nameInput.style.border = "1px solid #cc0000";
     return;
+  } else {
+    nameInput.placeholder = " ";
+    nameInput.style.border = "1px solid #dee2e6";
   }
 
   // verifica se o email está preenchido e se é válido
   if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
-    alert("Por favor, preencha o seu email.");
+    emailInput.placeholder = "Campo Obrigatório";
+    emailInput.style.border = "1px solid #cc0000";
     return;
+  } else {
+    emailInput.placeholder = " ";
+    emailInput.style.border = "1px solid #dee2e6";
   }
 
   // verifica s o numro de tefeone está preenchido
-  if (foneInput.value === "") {
-    alert("Por favor, informe o número de telefone para contato.");
+  if (foneInput.value === "" || foneInput.value === " ") {
+    foneInput.placeholder = "Campo Obrigatório";
+    foneInput.style.border = "1px solid #cc0000";
     return;
+  } else {
+    foneInput.placeholder = " ";
+    foneInput.style.border = "1px solid #dee2e6";
   }
 
   // verifica se a situaçao foi selcionada
   if (subjectSelect.value === "") {
-    alert("Por favor, selecione o assunto.");
+    subjectSelect.style.border = "1px solid #cc0000";
     return;
+  } else {
+    subjectSelect.style.border = "1px solid #dee2e6";
   }
 
   // verifica se a mensagem está preenchida
-  if (messageTextarea.value === "") {
-    alert("Por favor, disserte.");
+  if (messageTextarea.value === "" || messageTextarea.value === " ") {
+    messageTextarea.placeholder = "Campo Obrigatório";
+    messageTextarea.style.border = "1px solid #cc0000";
     return;
+  } else {
+    messageTextarea.placeholder = " ";
+    messageTextarea.style.border = "1px solid #dee2e6";
   }
 
   // envia o forms caso todos os campos estejam preenchidos
